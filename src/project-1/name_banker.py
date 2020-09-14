ace7386f46f1909fcedc49d437e19fd89e19b5c5
Severin Langberg <langberg91@gmail.com>
2020-09-14 10:50:31 +0200

    WIP: exp util from rain ex

--- src/project-1/name_banker.py
+++ src/project-1/name_banker.py
@@ -13,31 +13,65 @@ class NameBanker:
     	"""Return the action maximising expected utility."""
     	pass
 
-	def expected_utility(self):
+	def expected_utility(self, P, action, U):
 		"""Calculate the expected utility of a particular action for a given individual.
-
-		Args:
-			m: Investment.
-			n: Lending_period in months.
 		"""
 
-		# Credit worthy.
-		if self.predict_proba() > 0.5:
-			return m * ((1 + self.rate) ** n - 1)
+	    _, n_outcomes = np.shape(P)
+	    n_actions, _ = np.shape(U)
 
-		return -1.0 * m
+	    utility = 0
+	    for a in range(n_actions):
+	        for w in range(n_outcomes):
+
+	            P_w = get_proba(P, w)
+	            utility += U[action, w] * P_w
+
+	    return utility
+
+	def get_proba(self, P, w):
+
+		return 0.4
 
 	# predicting credit worthiness as input to your policy
 	def predict_proba(self):
 		"""The probability that a person will return the loan."""
-		pass
+			
+		# NB: Random seed. 
+		p_credit_worthy = np.random.choice(self.posterior)
 
 	# Fit the model to the data.  You can use any model you like to do
     # the fit, however you should be able to predict all class
     # probabilities
+    # for giving or denying credit to individuals
     def fit(self, X, y):
- 		"""Create a model estimating probabilities forpredict_proba()."""
-
-    	p_creditworthines = sum(credit_worthy) / total
+ 		"""Create a model estimating probabilities for predict_proba()."""
 
         self.data = [X, y]
+
+        likelihood = ...
+        prior = ...
+        self.posterior = likelihood * prior
+
+
+if __name__ == "__main__":
+
+	# TODO: Put whatever goes here in notebook.
+	n_models = 10
+	n_outcomes = 2
+	interest_rate = 0.017
+
+	P = np.zeros([n_models, n_outcomes])
+	prior = np.ones(n_models) / n_models
+
+	decision_maker = NameBanker()
+	decision_maker.set_interest_rate(interest_rate)
+
+	#for model in range(n_models):
+		# Likelihood form data.
+	#	P[model, 1] = 
+	#	P[model, 0] = 
+	#p_credit_worthy = name_banker.predict_proba()
+
+	#action = name_banker.get_best_action(belief, P, U)
+	#belief = name_banker.get_posterior(belief, P, ...)
