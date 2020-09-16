103141a70aa5655d405dfcfe6bbcfb4f5eefd9ba
Severin Langberg <langberg91@gmail.com>
2020-09-16 17:15:54 +0200

    BM: Implemented expected_utility() (v.1)

--- src/project-1/name_banker.py
+++ src/project-1/name_banker.py
@@ -1,77 +1,52 @@
 import numpy as np 
 
+
 class NameBanker:
-	"""
-	Args:
-		r: Interest rate. 
-	"""
 
-    def set_interest_rate(self, rate):
-        self.rate = rate
+	def set_interest_rate(self, rate):
 
-    def get_best_action(self):
-    	"""Return the action maximising expected utility."""
-    	pass
+		self.rate = rate
 
-	def expected_utility(self, P, action, U):
-		"""Calculate the expected utility of a particular action for a given individual.
-		"""
-
-	    _, n_outcomes = np.shape(P)
-	    n_actions, _ = np.shape(U)
-
-	    utility = 0
-	    for a in range(n_actions):
-	        for w in range(n_outcomes):
-
-	            P_w = get_proba(P, w)
-	            utility += U[action, w] * P_w
-
-	    return utility
-
-	def get_proba(self, P, w):
+	# TODO:
+	# TEMP: Dummy config 
+	def get_proba(self):
 
 		return 0.4
 
-	# predicting credit worthiness as input to your policy
-	def predict_proba(self):
-		"""The probability that a person will return the loan."""
-			
-		# NB: Random seed. 
-		p_credit_worthy = np.random.choice(self.posterior)
+	def expected_utility(self, x, action):
 
-	# Fit the model to the data.  You can use any model you like to do
-    # the fit, however you should be able to predict all class
-    # probabilities
-    # for giving or denying credit to individuals
-    def fit(self, X, y):
- 		"""Create a model estimating probabilities for predict_proba()."""
+		# Expected utility given some action.
+		utility = 0
+		for i in range(len(x)):
 
-        self.data = [X, y]
+			# No update unless granting loan.
+			if action == 1:
 
-        likelihood = ...
-        prior = ...
-        self.posterior = likelihood * prior
+				# Probability of being credit worthy.
+				Pi = self.get_proba()
+
+				n = x["duration"].iloc[i]
+				m = x["amount"].iloc[i]
+
+				utility += m * ((1 + self.rate) ** n - 1) * Pi - (1 - Pi) * m
+
+		return utility
 
 
 if __name__ == "__main__":
 
-	# TODO: Put whatever goes here in notebook.
-	n_models = 10
-	n_outcomes = 2
-	interest_rate = 0.017
+	import pandas
 
-	P = np.zeros([n_models, n_outcomes])
-	prior = np.ones(n_models) / n_models
+	target = ['repaid']
+	features = ['checking account balance', 'duration', 'credit history',
+	            'purpose', 'amount', 'savings', 'employment', 'installment',
+	            'marital status', 'other debtors', 'residence time',
+	            'property', 'age', 'other installments', 'housing', 'credits',
+	            'job', 'persons', 'phone', 'foreign']
+
+	df = pandas.read_csv('../../data/credit/german.data', sep=' ', names=features + target)
 
 	decision_maker = NameBanker()
-	decision_maker.set_interest_rate(interest_rate)
-
-	#for model in range(n_models):
-		# Likelihood form data.
-	#	P[model, 1] = 
-	#	P[model, 0] = 
-	#p_credit_worthy = name_banker.predict_proba()
-
-	#action = name_banker.get_best_action(belief, P, U)
-	#belief = name_banker.get_posterior(belief, P, ...)
+	decision_maker.set_interest_rate(0.05)
+	utility = decision_maker.expected_utility(df, 1)
+	print(utility)
