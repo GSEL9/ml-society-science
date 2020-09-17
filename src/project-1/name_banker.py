1b17808451df7f666102634142e08c20dac811f4
Markus SH <markuhei@ifi.uio.no>
2020-09-17 14:04:28 +0200

    Add my edits for namebanker

--- src/project-1/name_banker.py
+++ src/project-1/name_banker.py
@@ -1,43 +1,98 @@
-import numpy as np 
+import pandas as pd
+from collections import Counter
+from itertools import chain
+
+# for backwards portability, define prod if not in math module
+try:
+    from math import prod
+except ImportError:
+    import functools, operator
+    def prod(iterable):
+        return functools.reduce(operator.mul, iterable, 1)
 
 class NameBanker:
-	"""
-	Args:
-		r: Interest rate. 
-	"""
+    actions = (0, 1)
+    classes = (1, 2)
 
-    def set_interest_rate(self, rate):
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
+        """Docstring for fit"""
+        self.features = X
+        self.targets = y
+        self.N = len(X)
+
+        self.feature_frequencies = {col: Counter(X[col]) for col in X.columns}
+        print(self.feature_frequencies)
+        self.conditionals = {c: {col: Counter(X[y == c][col]) for col in X.columns} for c in self.classes}
+
+        self.class_frequencies = {c : len(y[y==c]) for c in self.classes}
+
+        # for counts in chain(self.feature_frequencies.values(), self.conditionals.values()):
+        for counts in self.feature_frequencies.values():
+            counts["sum"] = sum(counts.values())
+
+    def set_interest_rate(self, rate: int) -> None:
         self.rate = rate
 
-    def get_best_action(self):
-    	"""Return the action maximising expected utility."""
-    	pass
+    # predicting credit worthiness as input to your policy
+    def predict_proba(self, x: pd.DataFrame) -> float:
+        """The probability that a person will return the loan."""
 
-	def expected_utility(self):
-		"""Calculate the expected utility of a particular action for a given individual.
+        # p(c | x) = (p(x) * p(x | c))/p(c)
 
-		Args:
-			m: Investment.
-			n: Lending_period in months.
-		"""
+        # prod()
+        # class space:
+        # 1 == return_loan
+        # 2 == not_return_loan
+        p_c = self.class_frequencies[2]/self.N
 
-		# Credit worthy.
-		if self.predict_proba() > 0.5:
-			return m * ((1 + self.rate) ** n - 1)
+        # alpha to apply smoothing
+        alpha = 1.4e-9
 
-		return -1.0 * m
+        p_x = prod((self.feature_frequencies[col].get(x[col], 0) + alpha)
+                    / (self.feature_frequencies[col]["sum"] + alpha)
+                    for col in self.features.columns)
 
-	# predicting credit worthiness as input to your policy
-	def predict_proba(self):
-		"""The probability that a person will return the loan."""
-		pass
+        p_x_given_c = prod((self.conditionals[1][col].get(x[col], 0) + alpha)
+                            / (self.class_frequencies[1] + alpha)
+                            for col in self.features.columns)
 
-	# Fit the model to the data.  You can use any model you like to do
-    # the fit, however you should be able to predict all class
-    # probabilities
-    def fit(self, X, y):
- 		"""Create a model estimating probabilities forpredict_proba()."""
+        return (p_x * p_x_given_c)/p_c
 
-    	p_creditworthines = sum(credit_worthy) / total
+    def expected_utility(self, x: pd.DataFrame, action: int) -> float:
+        """Calculate the expected utility of a particular action for a given individual.
 
-        self.data = [X, y]
+        Args:
+            x: Features describing a selected individual
+            action: whether or not to give loan.
+        """
+
+        if "features" not in self.__dict__:
+            raise ValueError("This NameBanker instance is not fitted yet. Call 'fit' with appropriate arguments before using this method.")
+
+        n = x["duration"]
+        m = x["credits"]
+
+        if action:
+            p = self.predict_proba(x)
+            return p * m * ((1+self.rate)**n - 1)
+        else:
+            p = 1 - self.predict_proba(x)
+            return p * m
+
+    def get_best_action(self, x: pd.DataFrame) -> int:
+        """Return the action maximising expected utility.
+
+        Args:
+            x: Features describing a selected individual
+        Returns:
+            action: 0 or 1 regarding wether or not to give loan
+        """
+
+        # Referring to the dataset description in 'german.doc' :
+        #
+        # It is worse to class a customer as good when they are bad (5),
+        # than it is to class a customer as bad when they are good (1).
+
+        # TODO: weight the costs so that it selects the best action with respect
+        # to the uneven cost balance
+        return max(self.actions, key=lambda a: self.expected_utility(x, a))
