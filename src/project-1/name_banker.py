8a849ad6ea80ba0485ac83ecf505fbd3f6b84b97
Moeen Ali Naqvi <moeen.biit@gmail.com>
2020-09-25 11:12:58 +0200

    updated fit and predict_proba
    added features for name_banker

--- src/project-1/name_banker.py
+++ src/project-1/name_banker.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 
 
 class NameBanker:
@@ -11,17 +13,47 @@ class NameBanker:
     # actions = (0, 1)
     # classes = (1, 2)
 
-    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
-        """Docstring for fit"""
+    def __init__(self, classifier):
+        self.classifier = classifier
 
-        self.classifier = RandomForestClassifier(
-            n_estimators=100,
-            random_state=0,
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
+        """Fits a model for calculating the probability of credit- worthiness
+
+        Args:
+            X: Feature set of individuals
+            y: Target labels against the individuals
+
+        Returns:
+            None
+
+        """
+
+        self.scaler = StandardScaler()
+        X_scaled = self.scaler.fit_transform(X)
+
+        #Finding optimal paramters
+        param_grid = [{
+            'bootstrap' : [True],
+            'max_features' : list(range(10,20,1)),
+            'max_depth' : list(range(10,100,10)),
+            'n_estimators' : list(range(25,150,25))
+        }]
+
+        grid_search = GridSearchCV(estimator = RandomForestClassifier(), param_grid = param_grid, cv = 5)
+        grid_search.fit(X_scaled,y)
+
+        self.classifier = RandomForestClassifier(**grid_search.best_params_)
+
+        #self.classifier = RandomForestClassifier(
+            #n_estimators=100,
+            #random_state=42,
             # TODO: find best parameters
             # max_depth=self.best_max_depth,
             # max_features=self.best_max_features,
-            class_weight="balanced"
-        )
+            #class_weight="balanced"
+        #)
+        
+
         self.classifier.fit(X,y)
 
     def set_interest_rate(self, rate: float) -> None:
@@ -29,13 +61,26 @@ class NameBanker:
         self.rate = rate
 
     def predict_proba(self, x: pd.Series) -> float:
-        """Returns the probability that a person will return the loan."""
+        """Returns the probability that a person will return the loan.
+
+        Args:
+            x: Features describing a selected individual
+
+        Returns:
+            probability:
+                real number between 0 and 1 denoting probability of the individual to be credit-worthy
+
+        """
 
         if not hasattr(self, "classifier"):
             raise ValueError("This NameBanker instance is not fitted yet. Call 'fit' "
                              "with appropriate arguments before using this method.")
 
-        return self.classifier.predict_proba([x.to_numpy()])[0][1]
+        x_reshaped = np.reshape(x.to_numpy(), (1,-1))
+        
+        x_scaled = self.scaler.transform(x_reshaped)
+        
+        return self.classifier.predict_proba(x_scaled)[0][1]
 
     def expected_utility(self, x: pd.Series, action: int) -> float:
         """Calculate the expected utility of a particular action for a given individual.
